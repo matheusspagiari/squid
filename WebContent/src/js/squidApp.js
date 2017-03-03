4852bee03e4c6c4c99dcaa245d0526983a1a34b9
Matheus Spagiari <matheus_spagiari@hotmail.com>
2017-03-03 12:37:39 -0300

    Animação search bar

--- WebContent/src/js/squidApp.js
+++ WebContent/src/js/squidApp.js
@@ -1,1 +1,37 @@
 var squidApp = angular.module("squidApp",['ngMaterial'])
+
+squidApp.controller('searchController', function($scope) {
+	  $scope.data = data;
+
+	  $scope.setQuery = function(query) {
+	    $scope.query = query;
+	    $scope.focus = false;
+	  };
+	});
+
+	// Returns the search function that will perform the filter on the data.
+	//
+	squidApp.filter('search', function() {
+	  return search;
+	});
+
+	// Returns an array of items where the item text matches the search query. In
+	// this example, both the query and item are converted to lower case for easier
+	// matching.
+	//
+	function search(arr, query) {
+	  if (!query) {
+	    return arr;
+	  }
+
+	  var results = [];
+	  query = query.toLowerCase();
+
+	  angular.forEach(arr, function(item) {
+	    if (item.toLowerCase().indexOf(query) !== -1) {
+	      results.push(item);
+	    }
+	  });
+
+	  return results;
+	};
